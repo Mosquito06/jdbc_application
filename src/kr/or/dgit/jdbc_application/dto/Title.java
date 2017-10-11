d66111da012cd9e3732ade27c1a6119e9e59c8da
Mosquito(A) <skykim10908@naver.com>
2017-10-11 14:19:30 +0900

    20171011 dto 생성완료

---
+++ src/kr/or/dgit/jdbc_application/dto/Title.java
@@ -0,0 +1,27 @@
+package kr.or.dgit.jdbc_application.dto;
+
+public class Title {
+	private int title;
+	private String titleName;
+
+	public Title() {
+
+	}
+
+	public int getTitle() {
+		return title;
+	}
+
+	public void setTitle(int title) {
+		this.title = title;
+	}
+
+	public String getTitleName() {
+		return titleName;
+	}
+
+	public void setTitleName(String titleName) {
+		this.titleName = titleName;
+	}
+
+}
